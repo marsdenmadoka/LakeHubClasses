e0a616355f90eed68cb1eba7da8c416eecf183a6
marsdenmadoka <marsmadoka98@gmail.com>
2021-02-21 16:29:48 +0300

    added emebeded images and videos

--- lakehubcodes/todos/todo-app.js
+++ lakehubcodes/todos/todo-app.js
@@ -36,10 +36,12 @@ const renderTools=function(todosItem){
     const incompletedtodos = todosItem.filter(function(todo){
         return !todo.completed    
     })
-    const summary=document.createElement("h2")
-    summary.textContent=`you have ${incompletedtodos.length} todos left`
-    document.getElementById("todos").appendChild(summary)
 
+    const summary=document.createElement("h2") //h2 is a heading 
+    summary.textContent=`you have ${incompletedtodos.length} incomplete todos left`
+
+    document.getElementById("todos").appendChild(summary)
+           //displaying the incomplete todos
     incompletedtodos.forEach(function(incompletetodo){
     const p = document.createElement("p") //p is paragaraph
     p.textContent=incompletetodo.text;
