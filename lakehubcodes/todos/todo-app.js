d5c1ca83444b72bf959d63433afddfa2778f5a84
marsdenmadoka <marsmadoka98@gmail.com>
2021-03-03 10:11:32 +0300

    added position and floating elements

--- lakehubcodes/todos/todo-app.js
+++ lakehubcodes/todos/todo-app.js
@@ -1,53 +1,72 @@
-const todos =[
-    {
-    text:'Buy house',
-    completed:false
-},
+// const todos =[
+//     {
+//     text:'Buy house',
+//     completed:false
+// },
 
-{
-    text:'design',
-    completed:true
-},
-{
-    text:'my life as a shiling',
-    completed:false
-},
-{
-    text:'we hold the truths',
-    completed:false
-},
-{
-    text:'javscript is mine',
-    completed:true
-},
+// {
+//     text:'design',
+//     completed:true
+// },
+// {
+//     text:'my life as a shiling',
+//     completed:false
+// },
+// {
+//     text:'we hold the truths',
+//     completed:false
+// },
+// {
+//     text:'javscript is mine',
+//     completed:true
+// },
 
-]
+// ]
 
-const renderTools=function(todosItem){
-         //getting out todos
-    todosItem.forEach(function(todo){
-        const p = document.createElement("p") //p is paragaraph
-      p.textContent=todo.text;
-      document.getElementById("todos").appendChild(p) 
 
-    })
+// const renderTools=function(todosItem){
+//          //getting out todos
+//  todosItem.forEach(function(todo){
+//         const p = document.createElement("p") //p is paragaraph
+//       p.textContent=todo.text;
+//       document.getElementById("todos").appendChild(p) 
 
-         //getting the incomplete todos
-    const incompletedtodos = todosItem.filter(function(todo){
-        return !todo.completed    
-    })
+//     })
 
-    const summary=document.createElement("h2") //h2 is a heading 
-    summary.textContent=`you have ${incompletedtodos.length} incomplete todos left`
+//          //getting the incomplete todos
+//     const incompletedtodos = todosItem.filter(function(todo){
+//         return !todo.completed    
+//     })
 
-    document.getElementById("todos").appendChild(summary)
-           //displaying the incomplete todos
-    incompletedtodos.forEach(function(incompletetodo){
-    const p = document.createElement("p") //p is paragaraph
-    p.textContent=incompletetodo.text;
-    document.getElementById("todos").appendChild(p)
-    })
+//     const summary=document.createElement("h2") //h2 is a heading 
+//     summary.textContent=`you have ${incompletedtodos.length} incomplete todos left`
+
+//     document.getElementById("todos").appendChild(summary)
+//            //displaying the incomplete todos
+//     incompletedtodos.forEach(function(incompletetodo){
+//     const p = document.createElement("p") //p is paragaraph
+//     p.textContent=incompletetodo.text;
+//     document.getElementById("todos").appendChild(p)
+//     })
     
-};
-renderTools(todos)
-//https://eloquentjavascript.net/
+// };
+// renderTools(todos)
+// //https://eloquentjavascript.net/
+
+
+
+
+function find_max(nums) {
+     let max_num = Number.NEGATIVE_INFINITY; // smaller than all other numbers
+     for (let num of nums) {
+     if (num > max_num) {
+     // (Fill in the missing line here)
+    // num = max_num
+    max_num += num
+     }
+     }
+     return max_num;
+ }
+    
+
+//obj.sort((a, b) => (a > b ? -1 : 1))
---
+++ lakehubcodes/todos3/todo-app.js
@@ -0,0 +1,71 @@
+let todos = []; //object
+
+const filters = {
+  searchText: "",
+  hideCompleted: false,
+};
+
+const todosJSON = localStorage.getItem("todos");
+
+if (todosJSON !== null) {
+  todos = JSON.parse(todosJSON);
+}
+
+const renderTodos = function (todosItem, filterData) {
+  const filteredTodos = todosItem.filter(function (todo) {
+    const searchTextMatch = todo.text
+      .toLowerCase()
+      .includes(filterData.searchText.toLowerCase());
+    const hideCompletedMatch = !filters.hideCompleted || !todo.completed;
+
+    return searchTextMatch && hideCompletedMatch;
+  });
+ 
+  const inCompleteTodos = filteredTodos.filter(function (todo) {
+    return !todo.completed; 
+  });
+
+  document.getElementById("todos").innerHTML = "";
+
+  const summary = document.createElement("h2");//heading two size
+  summary.textContent = `You have ${inCompleteTodos.length} todos left`;//display our browser
+  document.getElementById("todos").appendChild(summary)
+
+
+  filteredTodos.forEach(function (todo) { //loop to displaying all todos
+    const p = document.createElement("p"); //paragraph 
+    p.textContent = todo.text;
+    document.getElementById("todos").appendChild(p);//dispalying in our html
+  });
+};
+
+renderTodos(todos, filters); //main line the life
+
+document
+  .getElementById("search-text")
+  .addEventListener("input", function (event) {
+    filters.searchText = event.target.value;
+    renderTodos(todos, filters);
+  });
+
+  
+document.getElementById("newtodo").addEventListener("submit", function (event) {
+  event.preventDefault()
+  todos.push({ //adding data to array
+    text: event.target.elements.text.value,//getting value from the text form id
+    completed: false,
+  });
+  localStorage.setItem("todos", JSON.stringify(todos));//temporary storage in Json form
+  renderTodos(todos, filters);
+  event.target.elements.text.value = "";
+});
+
+
+document
+  .getElementById("hide-completed")
+  .addEventListener("change", function (event) {//change a js function in the browser
+    filters.hideCompleted = event.target.checked; //assign the value of hide complete according to the event at the is check..check the console to understand beter the event
+    //is true when checked and false when unchecked
+    console.log(event)
+    renderTodos(todos, filters);
+  });
